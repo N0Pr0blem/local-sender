a5e9958e0c1c08b87e4374c2e3eb45c7058f8b86
n0pr0blem <k.bobkov1408@gmail.com>
2024-08-05 17:37:10 +0300

    Создал проект

---
+++ src/main/java/com/web/localsender/LocalSenderApplication.java
@@ -0,0 +1,13 @@
+package com.web.localsender;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocalSenderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LocalSenderApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/web/localsender/LocalSenderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.web.localsender;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocalSenderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
